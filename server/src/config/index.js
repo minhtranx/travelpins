b849d81c9404f108e090b2ed7677c6b052f631a4
minhtranx <minhtran.tech@outlook.com>
2020-12-12 14:12:23 +0700

    Fixing heroku

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -6,10 +6,12 @@ import path from 'path';
  * - Default values to prevent adhoc failure
  * - Use this object for intellisense
  */
-dotenv.config({
-	path: path.join(__dirname, `../../.env`),
-	example: path.join(__dirname, `../../.env.example`)
-});
+if (process.env.NODE_ENV !== 'production') {
+	dotenv.config({
+		path: path.join(__dirname, `../../.env`),
+		example: path.join(__dirname, `../../.env.example`)
+	});
+}
 
 const config = {
 	NODE_ENV: process.env.NODE_ENV || 'development',
