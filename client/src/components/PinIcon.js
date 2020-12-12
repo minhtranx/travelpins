59d8173f912f5d56dae4f82499f3270cc33a01c0
minhtranx <minhtran.tech@outlook.com>
2020-12-12 15:23:51 +0700

    Fix build errors

--- client/src/components/PinIcon.js
+++ client/src/components/PinIcon.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PlaceTwoTone from '@material-ui/icons/PlaceTwoTone';
 
-export default ({ size, color, onClick }) => (
+const PinIcon = ({ size, color, onClick }) => (
 	<PlaceTwoTone onClick={onClick} style={{ fontSize: size, color }} />
 );
+export default PinIcon;
